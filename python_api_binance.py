7c52209c6323d8507d4829cc4d3346e13bcd1994
mimus-assa <73607556+mimus-assa@users.noreply.github.com>
2022-12-13 17:17:50 -0600

    Update python_api_binance.py

--- python_api_binance.py
+++ python_api_binance.py
@@ -1,6 +1,3 @@
-from binance import Client
-from flask import Flask, request
-from binance.enums import *
 # Binance API setup
 api_key = ''
 api_secret = ''
@@ -12,6 +9,14 @@ app = Flask(__name__)
 
 @app.route('/market', methods=['POST'])
 def market():
+    """This route makes a market order in the Binance exchange
+    
+    Sample JSON:
+    {
+        "quantity": 10,
+        "side": "SIDE_BUY"
+    }
+    """
     data = request.get_json()
     quantity = data['quantity']
     side = data["side"]
@@ -22,12 +27,28 @@ def market():
     
     # buy margin loan
 
-    buy_loan = client.create_margin_order(symbol="BTCBUSD", side=side2, type="MARKET",quantity=quantity, sideEffectType="MARGIN_BUY",isIsolated=True)
+    buy_loan = client.create_margin_order(
+        symbol="BTCBUSD", 
+        side=side2, 
+        type="MARKET",
+        quantity=quantity, 
+        sideEffectType="MARGIN_BUY",
+        isIsolated=True
+    )
     return 'OK'
 
 
 @app.route('/trade_btc_busd_margin_long', methods=['POST'])
 def trade_btc_busd_margin_long():
+    """This route makes a long margin order in the Binance exchange
+    
+    Sample JSON:
+    {
+        "quantity": 10,
+        "price_a": 15000,
+        "price_b": 12000
+    }
+    """
     data = request.get_json()
     quantity = data['quantity']
     price_a = data['price_a']
@@ -35,23 +56,53 @@ def trade_btc_busd_margin_long():
     
     # buy margin loan
 
-    buy_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_BUY, type="MARKET",quantity=quantity, sideEffectType="MARGIN_BUY",isIsolated=True)
+    buy_loan = client.create_margin_order(
+        symbol="BTCBUSD", 
+        side=SIDE_BUY, 
+        type="MARKET",
+        quantity=quantity, 
+        sideEffectType="MARGIN_BUY",
+        isIsolated=True
+    )
   
     # loop until price reaches either price_a or price_b
     while True:
         ticker = client.get_ticker(symbol='BTCBUSD')
         current_price = float(ticker['lastPrice'])
         if current_price >= price_a:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_SELL, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_SELL, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
         elif current_price <= price_b:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_SELL, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_SELL, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
 
     return 'OK'
 
 @app.route('/trade_btc_busd_margin_short', methods=['POST'])
 def trade_btc_busd_margin_short():
+    """This route makes a short margin order in the Binance exchange
+    
+    Sample JSON:
+    {
+        "quantity": 10,
+        "price_a": 15000,
+        "price_b": 12000
+    }
+    """
     data = request.get_json()
     quantity = data['quantity']
     price_a = data['price_a']
@@ -59,17 +110,38 @@ def trade_btc_busd_margin_short():
     
     # sell margin loan
 
-    buy_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_SELL, type="MARKET",quantity=quantity, sideEffectType="MARGIN_BUY",isIsolated=True)
+    buy_loan = client.create_margin_order(
+        symbol="BTCBUSD", 
+        side=SIDE_SELL, 
+        type="MARKET",
+        quantity=quantity, 
+        sideEffectType="MARGIN_BUY",
+        isIsolated=True
+    )
   
     # loop until price reaches either price_a or price_b
     while True:
         ticker = client.get_ticker(symbol='BTCBUSD')
         current_price = float(ticker['lastPrice'])
         if current_price >= price_a:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_BUY, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_BUY, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
         elif current_price <= price_b:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_BUY, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_BUY, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
     return 'OK'
 
@@ -77,6 +149,15 @@ def trade_btc_busd_margin_short():
 
 @app.route('/trade_btc_busd_margin_short_oco', methods=['POST'])
 def trade_btc_busd_margin_short_oco():
+    """This route makes a short margin order with an One-Cancels-Other (OCO) order in the Binance exchange
+    
+    Sample JSON:
+    {
+        "quantity": 10,
+        "price_a": 15000,
+        "price_b": 12000
+    }
+    """
     data = request.get_json()
     quantity = data['quantity']
     price_a = data['price_a']
@@ -90,16 +171,39 @@ def trade_btc_busd_margin_short_oco():
         ticker = client.get_ticker(symbol='BTCBUSD')
         current_price = float(ticker['lastPrice'])
         if current_price >= price_a:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_BUY, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_BUY, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
         elif current_price <= price_b:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_BUY, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_BUY, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
     return 'OK'
 
 
 @app.route('/trade_btc_busd_margin_long_oco', methods=['POST'])
 def trade_btc_busd_margin_long_oco():
+    """This route makes a long margin order with an One-Cancels-Other (OCO) order in the Binance exchange
+    
+    Sample JSON:
+    {
+        "quantity": 10,
+        "price_a": 15000,
+        "price_b": 12000
+    }
+    """
     data = request.get_json()
     quantity = data['quantity']
     price_a = data['price_a']
@@ -111,10 +215,24 @@ def trade_btc_busd_margin_long_oco():
         ticker = client.get_ticker(symbol='BTCBUSD')
         current_price = float(ticker['lastPrice'])
         if current_price >= price_a:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_SELL, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_SELL, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
         elif current_price <= price_b:
-            sell_loan = client.create_margin_order(symbol="BTCBUSD", side=SIDE_SELL, type="MARKET",quantity=quantity , sideEffectType="AUTO_REPAY",isIsolated=True)
+            sell_loan = client.create_margin_order(
+                symbol="BTCBUSD", 
+                side=SIDE_SELL, 
+                type="MARKET",
+                quantity=quantity , 
+                sideEffectType="AUTO_REPAY",
+                isIsolated=True
+            )
             break
 
     return 'OK'
